3c305b941530ff7e60f64328e1bab53102e3b5fa
Jens Elofsson <jens.elofsson@arm.com>
2024-07-05 13:16:45 +0000

    First version of the benchmarking tool.
    Runs on Linux® and Android™.
    
    Tests the different variations of the matmul_clamp_f32_qai8dxp_qsi4cxp
    microkernel with user-specified m-, n-, and k-values.
    
    Signed-off-by: Jens Elofsson <jens.elofsson@arm.com>
    
    Approved-by: Jakub Sujak <jakub.sujak@arm.com>

--- benchmark/main.cpp
+++ benchmark/main.cpp
@@ -3,21 +3,68 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 //
-#include <benchmark/benchmark.h>
+
+#include <unistd.h>
 
 #include <cstdint>
 #include <cstdio>
+#include <cstdlib>
 
-template <class... Args>
-void hello_benchmark(benchmark::State& state, Args&&... args) {
-    volatile size_t a = 0;
-    for (auto _ : state) {
-        for (int i = 0; i < 100; i++) {
-            a++;
-        }
-    }
+#include "benchmark/matmul/matmul_f32.hpp"
+
+void print_usage(char* name) {
+    fprintf(stderr, "Usage:\n");
+    fprintf(stderr, "%s -m 13 -n 17 -k 18\n", name);
+    fprintf(stderr, "\n");
+    fprintf(stderr, "For additional options:\n");
+    fprintf(stderr, "%s --help\n", name);
 }
 
-BENCHMARK(hello_benchmark);
+int main(int argc, char** argv) {
+    ::benchmark::Initialize(&argc, argv);
 
-BENCHMARK_MAIN();
+    bool mflag = false;
+    bool nflag = false;
+    bool kflag = false;
+
+    size_t m = 0;
+    size_t n = 0;
+    size_t k = 0;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "m:n:k:")) != -1) {
+        switch (opt) {
+            case 'm':
+                m = atoi(optarg);
+                mflag = true;
+                break;
+            case 'n':
+                n = atoi(optarg);
+                nflag = true;
+                break;
+            case 'k':
+                k = atoi(optarg);
+                kflag = true;
+                break;
+            case '?':
+                // Fallthrough
+            default:
+                print_usage(argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    if (!mflag || !nflag || !kflag) {
+        print_usage(argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    kai_matmul matmul_f32;
+    for (int i = 0; i < num_ukernel_variants; i++) {
+        ::benchmark::RegisterBenchmark(ukernel_variants[i].name, matmul_f32, ukernel_variants[i], m, n, k);
+    }
+
+    ::benchmark::RunSpecifiedBenchmarks();
+    ::benchmark::Shutdown();
+    return 0;
+}
