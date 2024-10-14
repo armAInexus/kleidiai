a4baa60bbab67161cf3a7267b140acd26b9990f2
Guillaume Gardet <guillaume.gardet@arm.com>
2024-10-14 09:32:32 +0000

    Fixes to ease packaging
    Fixes to ease packaging and building.
    
    Signed-off-by: Guillaume Gardet <guillaume.gardet@arm.com>
    
    Approved-by: Anton Bondarenko <anton.bondarenko@arm.com>

--- cmake/FetchGTest.cmake
+++ cmake/FetchGTest.cmake
@@ -13,6 +13,7 @@ endif()
 fetchcontent_declare(googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG        v1.14.0
+    FIND_PACKAGE_ARGS NAMES GTest
 )
 
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
